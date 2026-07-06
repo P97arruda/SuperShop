55807bbbc3c537eea80e600566306a458133bb5f
pliniodev97@gmail.com <User@DESKTOP-20FGN2U>
2026-07-06 10:44:19 +0100

    Fist controller

---
+++ SuperShop/Data/DataConext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+
+namespace SuperShop.Data
+{
+    public class DataConext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataConext(DbContextOptions<DataConext> options) : base(options)
+        {
+
+        }
+    }
+}
