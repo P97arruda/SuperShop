c694f448e789213e60cb4b8c2c7e9890e318b93c
pliniodev97@gmail.com <User@DESKTOP-20FGN2U>
2026-07-06 15:46:23 +0100

    Modify products.

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -6,7 +6,9 @@ namespace SuperShop.Data.Entities
     public class Product
     {
         public int Id { get; set; }
-
+        
+        [Required]
+        [MaxLength(50, ErrorMessage = "The fiel {0} can contain {1} characters lenght.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -16,10 +18,10 @@ namespace SuperShop.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Available")]
         public bool Isvailable { get; set; }
