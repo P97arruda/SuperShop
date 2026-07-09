38328be13f79c4c7341120cfcce88872ad8b36cf
p97arruda <pliniodev97@gmail.com>
2026-07-09 13:41:20 +0100

    Generic repository

---
+++ SuperShop/Data/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using SuperShop.Data.Entities;
+
+namespace SuperShop.Data
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(DataConext conext) : base(conext)
+        {
+            
+        }
+    }
+}
