e72b1fb891836a39203f97fa9263a96c54d9422d
p97arruda <pliniodev97@gmail.com>
2026-07-13 15:48:19 +0100

    Add and edit images image on API Products User on API Products

--- SuperShop/Data/ProductRepository.cs
+++ SuperShop/Data/ProductRepository.cs
@@ -1,12 +1,20 @@
-﻿using SuperShop.Data.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+using System.Linq;
 
 namespace SuperShop.Data
 {
     public class ProductRepository : GenericRepository<Product>, IProductRepository
     {
+        private readonly DataConext _context;
         public ProductRepository(DataConext conext) : base(conext)
+        { 
+            _context = conext;
+        }
+
+        public IQueryable GetAllWithUsers()
         {
-            
+            return _context.Products.Include(p => p.User);
         }
     }
 }
